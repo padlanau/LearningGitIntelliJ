22bb85fd1b3965f5b83c357acf92cc23a589bd54
JoJo Padlan <padlanau@yahoo.com.au>
2022-07-20 13:37:38 +1000

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,5 +3,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
         System.out.println("AddedClass");        
+        System.out.println("UpdatedClass");
     }
 }
